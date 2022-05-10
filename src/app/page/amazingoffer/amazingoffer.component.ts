c0c83d659f014b3f321ddb264075431dc1c21104
faeze <faezeebold@gmail.com>
2022-05-10 17:57:50 +0430

    scroll

--- src/app/page/amazingoffer/amazingoffer.component.ts
+++ src/app/page/amazingoffer/amazingoffer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-amazingoffer',
@@ -10,20 +11,27 @@ export class AmazingofferComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    for (let index = 0; index < 2; index++) {
-      setTimeout(() => {
-        this.width -= 175
-      }, 1000);
-      
-    }
-    
   }
   width:number = 0
+ flag:boolean = true
 
-  show(){
+  showRight(){
     this.width -= 175
     console.log(this.width)
-
+    if(this.width < 0) {
+      this.flag = false
+    }else{
+      this.flag = true
+    }
   }
+  showLeft(){
+    this.width += 175
+    if(this.width < 0) {
+      this.flag = false
+    }else{
+      this.flag = true
+    }
+  }
+
 
 }
